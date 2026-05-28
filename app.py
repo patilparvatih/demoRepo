6a0ce4999d8c0f0ae17c8ac49b3e4321ab1d7bf5
. <.>
2026-05-28 17:45:20 +0530

    adding templates and index.html file and returning html template in response and passing title from python to html

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv
 from os import getenv
-from flask import Flask
-
+from flask import (
+    Flask , render_template
+)
+load_dotenv()
 PORT = int(getenv("PORT"))
-app = Flask(__name__),
+app = Flask(__name__,
+  template_folder = "templates"
+)
 
 @app.get("/")
 def home():
-    return "<h1>Welcome to Flask server</h1>"
+    return render_template("/index.html",title="Home")
 
 if __name__ == '__main__':
     app.run(debug=True,port=PORT)
